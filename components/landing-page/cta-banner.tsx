4046503446da9b35ab202b28d4c22d43f3c54cdb
andrew <andrewijaya077@gmail.com>
2026-02-03 13:31:57 +0700

    update link parthner with us

--- components/landing-page/cta-banner.tsx
+++ components/landing-page/cta-banner.tsx
@@ -17,7 +17,7 @@ export function CtaBanner() {
             <Link href={"https://form.fillout.com/t/mrMSiA7GuPus"}>Apply Now</Link>
           </button>
           <button className="px-10 py-4 border-2 border-white text-white font-bold rounded-lg hover:bg-white/10 transition-all">
-            <Link href={"/form/companies"}>Partner With Us</Link>
+            <Link href={"https://form.fillout.com/t/bazxKgfFaSus"}>Partner With Us</Link>
           </button>
         </div>
       </div>
